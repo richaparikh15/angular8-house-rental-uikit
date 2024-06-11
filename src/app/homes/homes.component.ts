dde7742054eaa0a7e8e68e4da58d09395d440eb9
richaparikh15 <46055833+richaparikh15@users.noreply.github.com>
2024-06-11 15:06:50 -0400

    custome event for the date added

--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Router, ActivatedRoute } from '@angular/router';
+declare var gtag;
 
 @Component({
   selector: 'app-homes',
@@ -36,4 +37,12 @@ export class HomesComponent implements OnInit {
     this.router.navigate(['homes'], { queryParams: { 'home-type': $event } });
   }
 
+  trackMe() {
+    gtag('event', 'click', {
+      'event_category': 'DATE_BUTTON_CLICK',
+      'event_label': 'Dates button cliked',
+      'value': 'Dates button has been clicked'   })
+  }
 }
+
+
